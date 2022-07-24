bb3bb07325ed9c51966d2245eb2ff8b959ac63d0
熊熊熊熊熊 <985049549@qq.com>
2022-07-24 14:20:38 +0800

    7-24 手写call方法

---
+++ 7-24_手写call.js
@@ -0,0 +1,23 @@
+// 手写call方法，并且不能使用bind、apply
+
+Function.prototype.myCall = function(ctx, ...arg){
+    // 如果没有制定对象，默认绑定在全局对象，如果是字符串或者数字，则转化为变量对象
+    ctx = (ctx === null || ctx === undefined) ? globalThis : Object(ctx)
+    // 将原函数挂载至上下文对象，并使用Symbol对象作为键，防止重复
+    var key = Symbol('temp')
+    // 把原函数挂载在上下文对象上，并使其隐藏不可便利
+    Object.defineProperty(ctx, key, {enumerable:false,value:this})
+    // 实现原函数的行为
+    var result = ctx[key](...arg)
+    // 删掉挂载的原函数
+    delete ctx[key]
+    // 返回原函数的结果
+    return result
+}
+
+function method(a,b){
+    console.log(this,a,b)
+    return a + b
+}
+
+method.myCall({},1,2)
